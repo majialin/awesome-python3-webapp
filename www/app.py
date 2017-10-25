edaac809c635efbe36f6215bdc927a3357a55f40
majialin <njumajialin@163.com>
2017-10-25 18:06:43 +0800

    web-frame start

--- www/app.py
+++ www/app.py
@@ -21,3 +21,53 @@ async def init(loop):
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+def get(path):
+    '''
+    Define decorator @get('/path')
+    :param path:
+    :return:
+    '''
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kw):
+            return func(*args, **kw)
+        wrapper.__method__ = 'GET'
+        wrapper.__route__ = path
+        return wrapper
+
+class RequestHandler(object):
+
+    def __init__(self, app, fn):
+        self._app = app
+        self._func = fn
+
+    async def __call__(self, request):
+        kw = request.__dict__
+        r = await self._func(**kw)
+        return r
+
+def add_route(app, fn):
+    method = getattr(fn, '__method__', None)
+    path = getattr(fn, '__route__', None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s.' % str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn = asyncio.coroutine(fn)
+    logging.info('add route %s %s => %s(%s)' % (method, path, fn.__name__, ', '.join(inspect.signature(fn).parameters.keys())))
+    app.route.add_route(method, path, RequestHandler(app, fn))
+
+def add_routes(app, module_name):
+    n = module_name.rfind('.')
+    if n == (-1):
+        mod = __import__(module_name, globals(), locals())
+    else:
+        name = module_name[n+1:]
+        mod = getattr(__import__(module_name[:n], globals(), localls(),[name]), name)
+    for attr in dir(mod):
+        if attr.startswith('_'):
+            continue
+        method = getattr(fn, '__method__', None)
+        path = getattr(fn, '__route__', None)
+        if  method and path:
+            add_route(app, fn)
